f621d480c6a1d6184b88168159010d7a1338c3ef
Luke Mclaren <luke@skiomusic.com>
2015-07-27 15:20:39 -0700

    Checking the clauses for correctness

--- s3.py
+++ s3.py
@@ -8,6 +8,21 @@ def readFile():
 	else:
 		print "\npython s3.py <sudoku input file>\n"
 
+def parseSolution():
+	f = open(sys.argv[1], 'r')
+	sArray = [line for line in f]
+	solution = sArray[1].split()
+	print "\n"
+	x = 0
+	for index, value in enumerate(solution):
+		if int(value) > 0:
+			x += 1
+			print (((int(value)))),
+			if x % 9 == 0:
+				print ""
+	print "\n"
+
+
 def constructCNF(sArray):
 
 	varCount = 0
@@ -32,27 +47,34 @@ def constructCNF(sArray):
 		for idI, row in enumerate(sArray):
 			for idJ, value in enumerate(row):
 				for x in range(8):
-					if(idJ < 8):
-						clauseCount += 1
-						print (-1*((int(idI) * 81) + (int(idJ) * 9) + int(x) + 1)),
-						print (-1*((int(idI) * 81) + (int(idJ+1) * 9) + int(x) + 1)),
-						print 0
+					for y in range(8):
+						y += 1
+						if(idJ+y < 9 and idJ < 8):
+							clauseCount += 1
+							print (-1*((int(idI) * 81) + (int(idJ) * 9) + int(x) + 1)),
+							print (-1*((int(idI) * 81) + (int(idJ+y) * 9) + int(x) + 1)),
+							print 0
 
 		for idI, row in enumerate(sArray):
 			for idJ, value in enumerate(row):
 				for x in range(8):
-					if(idI < 8):
-						clauseCount += 1
-						print (-1*((int(idI) * 81) + (int(idJ) * 9) + int(x) + 1)),
-						print (-1*((int(idI+1) * 81) + (int(idJ) * 9) + int(x) + 1)),
-						print 0
+					for y in range(8):
+						y += 1
+						if(idI+y < 9 and idI < 8):
+							clauseCount += 1
+							print (-1*((int(idI) * 81) + (int(idJ) * 9) + int(x) + 1)),
+							print (-1*((int(idI+y) * 81) + (int(idJ) * 9) + int(x) + 1)),
+							print 0
 
 		print "p cnf %s %s" % (varCount, clauseCount)
 	else:
 		print "sArray not intialized correctly\n"
 
 def main():
-	sArray = readFile() # Open sudoku file and parse into array
-	constructCNF(sArray)
+	if(sys.argv[1] == 'input.txt'):
+		sArray = readFile() # Open sudoku file and parse into array
+		constructCNF(sArray)
+	elif(sys.argv[1] == 'output.out'):
+		parseSolution()
 
 main()
