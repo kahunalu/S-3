e5a4a5458849f16513bec8507958d97c330f9bc6
Luke Mclaren <luke@skiomusic.com>
2015-07-28 15:41:37 -0700

    S3 Finally working Woot Woot!

--- s3.py
+++ s3.py
@@ -17,7 +17,12 @@ def parseSolution():
 	for index, value in enumerate(solution):
 		if int(value) > 0:
 			x += 1
-			print (((int(value)))),
+			result = ((int(value)%81)%9)
+			if result == 0:
+				print 9,
+			else:
+				print result,
+
 			if x % 9 == 0:
 				print ""
 	print "\n"
@@ -29,44 +34,86 @@ def constructCNF(sArray):
 	clauseCount = 0
 
 	if sArray:
-		for idI, row in enumerate(sArray):
-			for idJ, value in enumerate(row):
-				if value in ['0', '.', '*', '?']:
-					varCount += 9
-					clauseCount +=1
-					for x in range(9):
-						print ((int(idI) * 81) + (int(idJ) * 9) + int(x) + 1),
+		for i, line in enumerate(sArray):
+			for j, value in enumerate(line):
+				if value not in ['0', '.', '*', '?']:
+					clauseCount += 1
+					print (((i)*81)+((j)*9)+int(value)),
 					print 0
 
-				else:
-					varCount += 1
-					clauseCount +=1
-					print ((int(idI) * 81) + (int(idJ) * 9) + int(value)),
-					print 0
+		for i in range(9):
+			i += 1
+			for j in range(9):
+				j += 1
+				for k in range(9):
+					k += 1
+					print (((i-1)*81)+((j-1)*9)+k),
+				clauseCount += 1
+				print 0
 
-		for idI, row in enumerate(sArray):
-			for idJ, value in enumerate(row):
-				for x in range(9): #this damn 9
-					for y in range(8):
-						y += 1
-						if(idJ+y < 9 and idJ < 8):
-							clauseCount += 1
-							print (-1*((int(idI) * 81) + (int(idJ) * 9) + int(x) + 1)),
-							print (-1*((int(idI) * 81) + (int(idJ+y) * 9) + int(x) + 1)),
-							print 0
+		for i in range(9):
+			i += 1
+			for k in range(9):
+				k += 1
+				for j in range(8):
+					j += 1
+					l = j + 1
+					while l <= 9:
+						clauseCount += 1
+						print (-1*(((i-1)*81)+((j-1)*9)+k)),
+						print (-1*(((i-1)*81)+((l-1)*9)+k)),
+						print 0
+						l +=1
 
-		for idI, row in enumerate(sArray):
-			for idJ, value in enumerate(row):
-				for x in range(9): #is a bitch
-					for y in range(8):
-						y += 1
-						if(idI+y < 9 and idI < 8):
-							clauseCount += 1
-							print (-1*((int(idI) * 81) + (int(idJ) * 9) + int(x) + 1)),
-							print (-1*((int(idI+y) * 81) + (int(idJ) * 9) + int(x) + 1)),
-							print 0
+		for j in range(9):
+			j += 1
+			for k in range(9):
+				k += 1
+				for i in range(8):
+					i += 1
+					l = i + 1
+					while l <= 9:
+						clauseCount += 1
+						print (-1*(((i-1)*81)+((j-1)*9)+k)),
+						print (-1*(((l-1)*81)+((j-1)*9)+k)),
+						print 0
+						l +=1
 
-		print "p cnf %s %s" % (varCount, clauseCount)
+		for k in range(9):
+			k +=1
+			for a in range(3):
+				for b in range(3):				
+					for u in range (3):
+						u +=1
+						for v in range(2):
+							v += 1
+							w = v + 1
+							while w <= 3:
+								clauseCount += 1
+								print (-1*((((3*a)+u-1) * 81) + (((3*b)+v-1) * 9) + k)),
+								print (-1*((((3*a)+u-1) * 81) + (((3*b)+w-1) * 9) + k)),
+								print 0
+								w +=1
+
+		for k in range(9):
+			k +=1
+			for a in range(3):
+				for b in range(3):			
+					for u in range (2):
+						u += 1
+						for v in range(3):
+							v += 1
+							w = u + 1
+							while w <= 3:
+								for t in range(3):
+									t += 1
+									clauseCount += 1
+									print (-1*((((3*a)+u-1) * 81) + (((3*b)+v-1) * 9) + k)),
+									print (-1*((((3*a)+w-1) * 81) + (((3*b)+t-1) * 9) + k)),
+									print 0
+								w +=1
+
+		print "p cnf 729 %s" %  clauseCount
 	else:
 		print "sArray not intialized correctly\n"
 
